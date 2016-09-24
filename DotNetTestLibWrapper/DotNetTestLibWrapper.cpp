c341ccabb44f10ebc6700aa1db65b185f5e344cf
J. Caleb Wherry <caleb@calebwherry.com>
2016-09-24 19:48:42 -0400

    Commented out non-running example.

--- DotNetTestLibWrapper/DotNetTestLibWrapper.cpp
+++ DotNetTestLibWrapper/DotNetTestLibWrapper.cpp
@@ -26,7 +26,7 @@ using namespace std;
 // Reason: "Unhandled Exception: System.IO.FileLoadException: Could not load file or assembly 'DotNetTestLibWrapper, Version=1.0.6111.33189, Culture=neutral, PublicKeyToken=null' or one of its dependencies. Could not find or load a type. (Exception from HRESULT: 0x80131522) ---> System.TypeLoadException: Type '<Module>' from assembly 'DotNetTestLibWrapper, Version=1.0.6111.33189, Culture=neutral, PublicKeyToken=null' has a field of an illegal type.
 //          -- - End of inner exception stack trace-- -
 //          at CsharpTestApp.Program.Main(String[] args)"
-static auto GlobalData3 = gcnew ConcurrentDictionary<String^, String^>();
+//static auto GlobalData3 = gcnew ConcurrentDictionary<String^, String^>();
 
 // (4) Correct way to do it:
 ref struct GlobalData
